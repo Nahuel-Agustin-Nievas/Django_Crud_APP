b16e135d7e26f83e934307bc42e298c46ad2ae48
Nahuel Nievas <nievasnahuel@gmail.com>
2023-02-27 00:40:24 +0100

    login/logout module done

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('tasks/', views.tasks, name='tasks'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
 ]
